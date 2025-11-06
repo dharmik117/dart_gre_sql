e7f4170e7bf6eed60fb8ad906ba6436549bf3815
dharmik-hb <157466650+dharmik-hb@users.noreply.github.com>
2025-11-06 13:02:38 +0530

    managed some validations

--- lib/auth_helper.dart
+++ lib/auth_helper.dart
@@ -30,36 +30,70 @@ class AuthHelper {
     } else {
       final user = result.first.toColumnMap();
 
-
       if (user['user_password'] == hashedPassword) {
-        return {'message': 'Login successful', 'id': user['user_id']};
+        return {
+          'message': 'Login successful',
+          'id': user['user_id'],
+          'is_success': true,
+        };
       } else {
-        return {'message': 'Invalid password'};
+        return {
+          'message': 'Invalid password',
+          'is_success': false,
+        };
       }
     }
   }
 
-  Future<Map<String, dynamic>> signUp(
-      {required String email, required String password}) async {
+  Future<Map<String, dynamic>> signUp({
+    required String email,
+    required String password,
+  }) async {
     final connection = await _db;
+
+    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+    if (!emailRegex.hasMatch(email)) {
+      return {'message': 'Invalid email format'};
+    }
+
+    if (password.length < 6) {
+      // Example length check
+      return {'message': 'Password must be at least 6 characters'};
+    }
+
     final hashedPassword = _hashPassword(password);
 
-    final result = await connection.execute(
-      Sql.named(
-        '''
-    Insert into auth (email,user_password) values (@email,@user_password) returning  email,user_password
-    ''',
-      ),
-      parameters: {
-        'email': email,
-        'user_password': hashedPassword,
-      },
-    );
+    try {
+      final emailCheck = await connection.execute(
+          Sql.named(''''''
+              '''
+      SELECT email FROM auth WHERE email = @email
+    '''),
+          parameters: {'email': email});
 
-    if (result.isEmpty) {
-      return {'message': 'Failed to create user'};
-    } else {
-      return result.first.toColumnMap();
+      if (emailCheck.isNotEmpty) {
+        return {'message': 'User already exists'};
+      }
+
+      final result = await connection.execute(
+        Sql.named('''
+        INSERT INTO auth (email, user_password)
+        VALUES (@email, @user_password)
+        RETURNING email, user_password
+      '''),
+        parameters: {
+          'email': email,
+          'user_password': hashedPassword,
+        },
+      );
+
+      if (result.isEmpty) {
+        return {'message': 'Failed to create user'};
+      } else {
+        return result.first.toColumnMap(); // Returns new user data
+      }
+    } catch (e) {
+      return {'message': 'Error occurred: $e'};
     }
   }
 
